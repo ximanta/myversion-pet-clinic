46d7c3d219fc89d54097f1cc9a3eac5cc83b410e
szoggun <przrut4@gmail.com>
2022-03-15 14:36:41 +0100

    Initial commit

---
+++ src/main/java/pl/przemyslawrutkowski/myversionpetclinic/MyversionPetClinicApplication.java
@@ -0,0 +1,13 @@
+package pl.przemyslawrutkowski.myversionpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyversionPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyversionPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/przemyslawrutkowski/myversionpetclinic/MyversionPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.przemyslawrutkowski.myversionpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyversionPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
